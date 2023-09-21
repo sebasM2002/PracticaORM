f312e979abdf160b04242ad3006cc0ec55c2c23a
sebas <sebastian.mercado1415@gmail.com>
2023-09-21 10:48:15 -0400

    ORMPractica Migrations Done

--- models.py
+++ models.py
@@ -1,24 +1,29 @@
-from sqlalchemy import create_engine, Column, Integer, String, Date, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, String, Date, ForeignKey, Boolean
 from sqlalchemy.orm import relationship
 from db import Base
 
 class Store(Base):
-    __tablename__ = "stores"
+    __tablename__ = "Store"
 
     id = Column(Integer, primary_key=True)
     name = Column(String(50))
+
+
+class Employee(Base):
+    __tablename__ = "Employee"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+
 
 class Inventory(Base):
-    __tablename__ = "inventories"
+    __tablename__ = "Inventory"
 
     id = Column(Integer, primary_key=True)
-    store_id = Column(Integer, ForeignKey("stores.id"))
-    flavor = Column(String(50))
+    store_id = Column(Integer, ForeignKey("store.id"))
+    employee_id = Column(Integer,ForeignKey("Employee.id"))
     date = Column(Date)
+    flavor = Column(String(50))
+    is_season_flavor = Column(Boolean)
     quantity = Column(Integer)
 
-class Flavor(Base):
-    __tablename__ = "flavors"
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String(50))

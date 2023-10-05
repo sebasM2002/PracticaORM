deafb2a2c0a0cfc66e615288cdee568684ec106b
gregorio-estevez <gregoriomestevez12@gmail.com>
2023-10-05 17:00:52 -0400

    graphQL implemented

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import session, sessionmaker, scoped_session
+from sqlalchemy.orm import session, sessionmaker
 from dotenv import load_dotenv
 import os
 
@@ -13,10 +13,9 @@ engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, expire_on_commit=False)
 
 Base = declarative_base()
-
 
 class DBContext:
     def __init__(self):
--- schema.py
+++ schema.py
@@ -1,70 +1,176 @@
 import graphene
 from graphene import relay
-from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
-from db import SessionLocal 
+from db import SessionLocal
 from models import Store as storeModel, Employee as employeeModel, Inventory as inventoryModel
 
-""" class Store(SQLAlchemyObjectType):
+
+class Store(graphene.ObjectType):
+    id = graphene.ID(required=True)
+    name = graphene.String()
     class Meta:
         model = storeModel
         interfaces = (relay.Node, )
 
-
-class Employee(SQLAlchemyObjectType):
+class Employee(graphene.ObjectType):
+    id = graphene.ID(required=True)
+    name = graphene.String()
     class Meta:
         model = employeeModel
-        interfaces = (relay.Node, )
+        interfaces = (relay.Node,)
 
-class Inventory(SQLAlchemyObjectType):
+class Inventory(graphene.ObjectType):
+    id = graphene.ID(required=True)
+    flavor = graphene.String()
+    store_id = graphene.String()
+    employee_id = graphene.String()
+    date = graphene.String()
+    is_season_flavor = graphene.Boolean()
+    quantity = graphene.Int()
     class Meta:
         model = inventoryModel
-        interfaces = (relay.Node, )
+        interfaces = (relay.Node,)
         
-class Query(graphene.ObjectType):
-    node = relay.Node.Field()
-     """
-     
-class Icecream(graphene.ObjectType):
+class FlavorInput(graphene.InputObjectType):
     flavor = graphene.String()
-    count = graphene.Int()
-    is_season_flavor = graphene.Boolean()
-
-class Store(graphene.ObjectType):
-    name = graphene.String()
-
-class Employee(graphene.ObjectType):
-    name = graphene.String()
-     
-class Inventory(graphene.ObjectType):
-    store = graphene.Field(Store)
-    employee = graphene.Field(Employee)
-    icecream = graphene.Field(Icecream)
-    date = graphene.String()
-# Inventory(out.to_dict())
-class IcecreamInput(graphene.InputObjectType):
-    flavor = graphene.String()
-    count = graphene.Int()
+    quantity = graphene.Int()
     is_season_flavor = graphene.Boolean()
 
 class StoreInput(graphene.InputObjectType):
     name = graphene.String()
+    id = graphene.ID()
 
 class EmployeeInput(graphene.InputObjectType):
     name = graphene.String()
+    id = graphene.ID()
 
 class InventoryInput(graphene.InputObjectType):
-    store = graphene.NonNull(StoreInput)
-    employee = graphene.NonNull(EmployeeInput)
-    icecream = graphene.NonNull(IcecreamInput)
+    store_id = graphene.NonNull(StoreInput)
+    employee_id = graphene.NonNull(EmployeeInput)
+    flavor = graphene.NonNull(FlavorInput)
     date = graphene.String()
-
+        
 class Query(graphene.ObjectType):
+    node = relay.Node.Field()
     inventory = graphene.List(Inventory, required=True)
     inventoryByFlavor = graphene.List(Inventory, required=True, flavor=graphene.String(required=True))
     inventoryByStore = graphene.List(Inventory, required=True, Store=graphene.String(required=True))
     inventoryByDate = graphene.List(Inventory, required=True, date=graphene.String(required=True))
+    
+    def resolve_inventory(self, info):
+        session = SessionLocal()
+        try:
+
+            inventory_data = session.query(inventoryModel).all()
+
+            inventory_list = [
+                Inventory(
+                    id=item.id,
+                    flavor=item.flavor,
+                    store_id=item.store_id,
+                    employee_id=item.employee_id,
+                    date=item.date,
+                    is_season_flavor=item.is_season_flavor,
+                    quantity = item.quantity,
+                )
+                for item in inventory_data
+            ]
+            return inventory_list
+        finally:
+            session.close()
+
+    def resolve_inventoryByFlavor(self, info, flavor):
+        session = SessionLocal()
+        try:
+            inventory_data = session.query(inventoryModel).filter(inventoryModel.flavor == flavor).all()
+            inventory_list = [
+                Inventory(
+                    id=item.id,
+                    flavor=item.flavor,
+                    store_id=item.store_id,
+                    employee_id=item.employee_id,
+                    date=item.date,
+                    is_season_flavor=item.is_season_flavor,
+                    quantity = item.quantity,
+                )
+                for item in inventory_data
+            ]
+            return inventory_list
+        finally:
+            session.close()
+
+    def resolve_inventoryByStore(self, info, store):
+        session = SessionLocal()
+        try:
+            inventory_data = session.query(inventoryModel).filter(inventoryModel.store_id == store).all()
+            
+            inventory_list = [
+                Inventory(
+                    id=item.id,
+                    flavor=item.flavor,
+                    store_id=item.store_id,
+                    employee_id=item.employee_id,
+                    date=item.date,
+                    is_season_flavor=item.is_season_flavor,
+                    quantity = item.quantity,
+                )
+                for item in inventory_data
+            ]
+            return inventory_list
+        finally:
+            session.close()
+
+    def resolve_inventoryByDate(self, info, date):
+        session = SessionLocal()
+        try:
+            inventory_data = session.query(inventoryModel).filter(inventoryModel.date == date).all()
+            
+            inventory_list = [
+                Inventory(
+                    id=item.id,
+                    flavor=item.flavor,
+                    store_id=item.store_id,
+                    employee_id=item.employee_id,
+                    date=item.date,
+                    is_season_flavor=item.is_season_flavor,
+                    quantity = item.quantity,
+                )
+                for item in inventory_data
+            ]
+            return inventory_list
+        finally:
+            session.close()
 
 class Mutation(graphene.ObjectType):
     addInventory = graphene.Field(Inventory, inventory=graphene.Argument(InventoryInput, required=True))
+    
+    def resolve_addInventory(self, info, inventory):
+        try:
+            store_data = inventory.store_id.id
+            employee_data = inventory.employee_id.id
+            flavor_data = inventory.flavor.flavor
+            date_data = inventory.date
+            season_data = inventory.flavor.is_season_flavor
+            quantity_data = inventory.flavor.quantity
+            
+            session = SessionLocal()
 
-schema = graphene.Schema(query=Query ,  mutation=Mutation)
+            new_inventory = inventoryModel(
+                store_id=store_data,
+                employee_id=employee_data,
+                flavor=flavor_data,
+                date=date_data,
+                is_season_flavor = season_data,
+                quantity = quantity_data,
+            )
+
+            session.add(new_inventory)
+            session.commit()
+
+            return new_inventory
+        except Exception as e:
+            session.rollback()
+            raise e
+        finally:
+            session.close()
+
+schema = graphene.Schema(query=Query ,  mutation=Mutation) 
